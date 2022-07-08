459dfba4fe301172b1f4c3e3bbc942f17a0d0303
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-07-08 14:24:33 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
+import { Content} from './components/Content';
 
 import { api } from './services/api';
 
